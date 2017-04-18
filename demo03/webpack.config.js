d2aa837f2a5245ddbb6145b4cb9001e58639d44f
yichengjie <626659321@qq.com>
2017-04-18 14:50:21 +0800

    项目结构调整

---
+++ demo03/webpack.config.js
@@ -0,0 +1,37 @@
+var path = require('path');
+
+module.exports = {
+    entry:"./src/index.tsx",
+    output: {
+        filename: 'index.js',
+        path: __dirname + "/public/dist"
+    },
+    devtool: "source-map",
+    resolve: {
+        // Add '.ts' and '.tsx' as a resolvable extension.
+        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+    },
+    preLoaders: [
+        // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+        { test: /\.js$/, loader: "source-map-loader" }
+    ],
+    module: {
+        loaders: [
+           // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            {test: /\.tsx?$/, loader: "awesome-typescript-loader"},
+            {
+                test: /\.js$/, 
+                loader: "babel",
+                include:  [
+                    path.resolve(__dirname,'./src')
+                ],
+                exclude: /node_modules/
+            }
+        ]
+    },
+    
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    },
+};
