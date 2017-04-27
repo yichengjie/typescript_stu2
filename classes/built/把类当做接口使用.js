1e179fa25283f966815b2ab132cfb5634a6d539e
yichengjie <626659321@qq.com>
2017-04-27 13:27:29 +0800

    s

--- classes/built/把类当做接口使用.js
+++
@@ -1,6 +0,0 @@
-var Point = (function () {
-    function Point() {
-    }
-    return Point;
-}());
-var point3d = { x: 1, y: 2, z: 3 };
--- namespace-module01/built/Test.js
+++
@@ -1,9 +0,0 @@
-var strings = ["Hello", "98052", "101"];
-var validators = {};
-validators["ZIP code"] = new Validation.ZipCodeValidator();
-validators["Leggers only"] = new Validation.LettersOnlyValidator();
-strings.forEach(function (s) {
-    for (var name_1 in validators) {
-        console.log("\"" + s + "\" " + (validators[name_1].isAcceptable(s) ? "matches" : "does not match " + name_1));
-    }
-});
