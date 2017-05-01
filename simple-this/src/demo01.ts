9ccab299abffbe8a8496d765b31aacb964a54fc3
yichengjie <626659321@qq.com>
2017-05-01 15:40:36 +0800

    this study

---
+++ simple-this/src/demo01.ts
@@ -0,0 +1,19 @@
+function foo(num){
+    console.info(`foo : ${num}`) ;
+    //记录foo被调用的次数
+    data.count ++ ;
+}
+
+var data = {
+    count : 0 
+} ;
+
+var i ;
+
+for(i = 0 ; i < 10 ; i ++){
+    if(i > 5){
+        foo(i) ;
+    }
+}
+
+console.info(data.count) ;
